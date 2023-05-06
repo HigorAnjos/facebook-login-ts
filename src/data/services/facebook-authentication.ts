11209fc313bf829d89437b9f5e42cf032588e423
Higor Anjos <higorc.anjos@gmail.com>
2023-05-06 17:17:59 -0300

    remove implementation details from serivce

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -1,27 +1,24 @@
 import { type LoadFacebookUserApi } from '@/data/Contracts/apis'
-import { type LoadUserAccountRepository, type CreateFacebookAccountRepository, type UpdateFacebookAccountRepository } from '@/data/Contracts/repos'
+import { type LoadUserAccountRepository, type SaveFacebookAccountRepository } from '@/data/Contracts/repos'
 import { AuthenticationError } from '@/domain/errors'
 import { type FacebookAuthentication } from '@/domain/features'
 
 export class FacebookAuthenticationService {
   constructor (
     private readonly loadFacebookUser: LoadFacebookUserApi,
-    private readonly userAccountRepo: LoadUserAccountRepository & CreateFacebookAccountRepository & UpdateFacebookAccountRepository
+    private readonly userAccountRepo: LoadUserAccountRepository & SaveFacebookAccountRepository
   ) {}
 
   async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
     const fbDate = await this.loadFacebookUser.loadUser(params)
     if (fbDate !== undefined) {
       const accoutData = await this.userAccountRepo.load({ email: fbDate.email })
-      if (accoutData !== undefined) {
-        await this.userAccountRepo.updateWithFacebook({
-          id: accoutData.id,
-          name: accoutData.name ?? fbDate.name,
-          facebookId: fbDate.facebookId
-        })
-      } else {
-        await this.userAccountRepo.createFromFacebook(fbDate)
-      }
+      await this.userAccountRepo.saveWithFacebook({
+        id: accoutData?.id,
+        name: accoutData?.name ?? fbDate.name,
+        email: fbDate.email,
+        facebookId: fbDate.facebookId
+      })
     }
     return new AuthenticationError()
   }
