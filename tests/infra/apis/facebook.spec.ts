b1c0f517081052666f277734bd6a09fcef442dce
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2023-06-29 22:38:58 -0300

    ensure FacebookApi gets debug token

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -15,6 +15,7 @@ describe('Facebook Api', () => {
   })
 
   beforeEach(() => {
+    httpClient.get.mockResolvedValue({ access_token: 'any_app_token' })
     sut = new FacebookApi(httpClient, clientId, clientSecret)
   })
 
@@ -27,6 +28,18 @@ describe('Facebook Api', () => {
         client_id: clientId,
         client_secret: clientSecret,
         grant_type: 'client_credentials'
+      }
+    })
+  })
+
+  it('should get debug token', async () => {
+    await sut.loadUser({ token: 'any_client_token' })
+
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/debug_token',
+      params: {
+        access_token: 'any_app_token',
+        input_token: 'any_client_token'
       }
     })
   })
