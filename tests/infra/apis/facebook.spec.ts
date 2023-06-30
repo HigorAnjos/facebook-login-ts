0ee3792899f3a23474d49f24f16c08468d247c11
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2023-06-29 22:59:52 -0300

    ensure FacebookApi gets user info

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -15,7 +15,10 @@ describe('Facebook Api', () => {
   })
 
   beforeEach(() => {
-    httpClient.get.mockResolvedValue({ access_token: 'any_app_token' })
+    httpClient.get
+      .mockResolvedValueOnce({ access_token: 'any_app_token' })
+      .mockResolvedValueOnce({ data: { user_id: 'any_user_id' } })
+      .mockResolvedValueOnce({ id: 'any_fb_id', name: 'any_fb_name', email: 'any_fb_email' })
     sut = new FacebookApi(httpClient, clientId, clientSecret)
   })
 
@@ -40,6 +43,18 @@ describe('Facebook Api', () => {
       params: {
         access_token: 'any_app_token',
         input_token: 'any_client_token'
+      }
+    })
+  })
+
+  it('should get user info', async () => {
+    await sut.loadUser({ token: 'any_client_token' })
+
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/any_user_id',
+      params: {
+        fields: 'id,name,email',
+        access_token: 'any_client_token'
       }
     })
   })
