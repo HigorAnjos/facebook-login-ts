6a9449d30fcb19557e86912586d09e16fcefdb96
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2023-07-01 14:53:45 -0300

    move AxiosHttpClient to its own file

--- tests/infra/apis/http/axios-client.spec.ts
+++ tests/infra/apis/http/axios-client.spec.ts
@@ -1,34 +1,50 @@
-import { type HttpGetClient } from '@/infra/http'
+
+import { AxiosHttpClient } from '@/infra/http'
+
 import axios from 'axios'
 
 jest.mock('axios')
 
-class AxiosHttpClient {
-  async get (args: HttpGetClient.Params): Promise<void> {
-    await axios.get(args.url, { params: args.params })
-  }
-}
-
 describe('AxiosHttpClient', () => {
+  let sut: AxiosHttpClient
+  let fakeAxios: jest.Mocked<typeof axios>
+  let url: string
+  let params: object
+
+  beforeAll(() => {
+    url = 'any_url'
+    params = { any: 'any' }
+    fakeAxios = axios as jest.Mocked<typeof axios>
+    fakeAxios.get.mockResolvedValue({
+      status: 200,
+      data: 'any_data'
+    })
+  })
+
+  beforeEach(() => {
+    sut = new AxiosHttpClient()
+  })
+
   describe('get', () => {
-    it('should call get with correct params', async () => {
-      const fakeAxios = axios as jest.Mocked<typeof axios>
-      const sut = new AxiosHttpClient()
+    it('should call get with correct input', async () => {
+      await sut.get({ url, params })
 
-      await sut.get({
-        url: 'any_url',
-        params: {
-          any: 'any'
-        }
-      })
-
-      expect(fakeAxios.get).toHaveBeenCalledWith('any_url', {
-        params: {
-          any: 'any'
-        }
-      })
-
+      expect(fakeAxios.get).toHaveBeenCalledWith(url, { params })
       expect(fakeAxios.get).toHaveBeenCalledTimes(1)
+    })
+
+    it('should return data on success', async () => {
+      const result = await sut.get({ url, params })
+
+      expect(result).toEqual('any_data')
+    })
+
+    it('should rethrow if get throws', async () => {
+      fakeAxios.get.mockRejectedValueOnce(new Error('http_error'))
+
+      const promise = sut.get({ url, params })
+
+      await expect(promise).rejects.toThrow(new Error('http_error'))
     })
   })
 })
