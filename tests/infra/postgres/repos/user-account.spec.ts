b98bf036e1fde4f222d5e80c24d9ff237f7abae6
anjos <cd>
2023-09-17 12:09:10 -0300

    move inMemoryDb creation to its own file

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -1,34 +1,8 @@
-import { type LoadUserAccountRepository } from '@/data/Contracts/repos'
-import { type IBackup, newDb } from 'pg-mem'
-import { Entity, PrimaryGeneratedColumn, Column, getRepository, type Repository, getConnection } from 'typeorm'
-
-class PgUserAccountRepository implements LoadUserAccountRepository {
-  async load (params: LoadUserAccountRepository.Params): Promise<LoadUserAccountRepository.Result> {
-    const pgUserRepo = getRepository(PgUser)
-    const pgUser = await pgUserRepo.findOne({ email: params.email })
-    if (pgUser !== undefined) {
-      return {
-        id: pgUser?.id?.toString(),
-        name: pgUser.name ?? undefined
-      }
-    }
-  }
-}
-
-@Entity({ name: 'usuarios' })
-class PgUser {
-  @PrimaryGeneratedColumn()
-    id!: number
-
-  @Column({ name: 'nome', nullable: true })
-    name?: string
-
-  @Column()
-    email!: string
-
-  @Column({ name: 'id_facebook', nullable: true })
-    facebookId?: string
-}
+import { PgUserAccountRepository } from '@/infra/postgres/repos'
+import { PgUser } from '@/infra/postgres/entities'
+import { type IBackup } from 'pg-mem'
+import { getRepository, type Repository, getConnection } from 'typeorm'
+import { makeFakeDb } from '../mocks/index'
 
 describe('PgUserAAccountRepository', () => {
   describe('load', () => {
@@ -37,12 +11,7 @@ describe('PgUserAAccountRepository', () => {
     let backup: IBackup
 
     beforeAll(async () => {
-      const db = newDb()
-      const connection = await db.adapters.createTypeormConnection({
-        type: 'postgres',
-        entities: [PgUser]
-      })
-      await connection.synchronize()
+      const db = await makeFakeDb()
       backup = db.backup() // gera um ponto de restauracao (Banco limpo)
       pgUserRepo = getRepository(PgUser)
     })
