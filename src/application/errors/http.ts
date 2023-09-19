316ee90fd27d5f69c76238b9e880df39362a2585
anjos <cd>
2023-09-18 23:31:09 -0300

    apply badRequest helper

--- src/application/errors/http.ts
+++ src/application/errors/http.ts
@@ -7,3 +7,10 @@ export class ServerError extends Error {
     }
   }
 }
+
+export class RequiredFieldError extends Error {
+  constructor (fieldName: string) {
+    super(`The field ${fieldName} is required`)
+    this.name = 'RequiredFieldError'
+  }
+}
--- src/application/helpers/http.ts
+++ src/application/helpers/http.ts
@@ -2,3 +2,13 @@ export type HttpResponse = {
   statusCode: number
   data: any
 }
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  data: error
+})
+
+export const unau = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  data: error
+})
